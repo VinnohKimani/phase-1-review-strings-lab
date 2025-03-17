d65ca4233dee4516a61a1690b39cb2fabac75823
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-03-17 12:59:01 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Kimani Macharia";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+console.log(shortGreeting)
